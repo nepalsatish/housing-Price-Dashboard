1b986d35ceeeff52beb7a8d0ba16f4a2ad760d6e
Satish Nepal <nepal.satish@hotmail.com>
2025-06-20 22:05:07 -0400

    app.py update host 0.0.0.0

--- housing_backend/app.py
+++ housing_backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from model_utils import get_data, load_model
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -184,4 +185,4 @@ def get_housing_data():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
